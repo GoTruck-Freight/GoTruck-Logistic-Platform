9693fcff3c65eb5ba9e1f5a613602826856dd09b
Shafag42 <shefeqa24@gmail.com>
2024-01-19 23:22:11 +0400

    Created microservices for GoTruck Logistics Platform

---
+++ service-registry/src/test/java/com/gotruck/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotruck.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
