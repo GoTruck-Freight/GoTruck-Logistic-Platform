59ab84dd54f7f3420cdcacd3c384a1d8d464d55e
Shafag42 <shefeqa24@gmail.com>
2024-03-25 00:17:23 +0400

    Added Prometheus and Grafana services for monitoring

--- shipper-service/src/main/java/com/gotruck/shipperservice/ShipperServiceApplication.java
+++ shipper-service/src/main/java/com/gotruck/shipperservice/ShipperServiceApplication.java
@@ -2,11 +2,12 @@ package com.gotruck.shipperservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ComponentScan
+@ComponentScan(basePackages = "com.gotruck.shipperservice")
+@PropertySource("classpath:env.properties")
 public class ShipperServiceApplication {
 
 	public static void main(String[] args) {
